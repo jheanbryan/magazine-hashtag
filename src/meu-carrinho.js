02dee293cebd086514a0c5b6b3d3ce2165d77a36
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2023-09-01 12:24:59 -0400

    adicionado carrinho e interatividade com ele

--- src/meu-carrinho.js
+++ src/meu-carrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades";
+
 function abrirCarrinho(){
     document.getElementById('carrinho').style.right = '0';
 }
@@ -8,6 +10,37 @@ function fecharCarrinho(){
 
 export function inicializarCarrinho(){
     document.getElementById('btnAbrirCarrinho').addEventListener('click', abrirCarrinho);
-
     document.getElementById('btnFecharCarrinho').addEventListener('click', fecharCarrinho);
+
 } 
+
+export function adicionarAoCarrinho(idProduto){
+    const produto = catalogo.find(p => p.id === idProduto);
+    const containerProdtudosCarrinho = document.getElementById('produtos-carrinho');
+    const cardProdutoCarrinho = `
+    <div class="card-produto-carrinho">
+
+        <div class="container-produto-carrinho">
+        <div class="div-img-produto-carrinho">
+            <img src=${produto.srcImg} alt="processador athlon3000g" class="img-produto-carrinho">
+        </div>
+        
+        <div class="info-produto-carrinho">
+            <span>${produto.nome}</span>
+            <span>R$ ${produto.valor}</span>
+        </div>
+        </div>  
+
+        <div class="container-remove-add">
+            <img src="assets/icons/circle-xmark-regular.svg" alt="" class="icon">   
+
+            <div class="menos-mais-btns">
+                <button>-</button>
+                <p>3 </p>
+                <button>+</button>
+            </div>
+        </div>
+  </div>
+    `;
+    containerProdtudosCarrinho.innerHTML += cardProdutoCarrinho
+}
---
+++ src/utilidades.js
@@ -0,0 +1,59 @@
+export const catalogo =[
+    {   
+        id:1,
+        nome: "Atlhon 3000G",
+        marca: "AMD",
+        valor: 500,
+        srcImg: "assets/cpus/athlon3000g.png"
+    },
+    {
+        id:2,
+        nome: "I3 10100F",
+        marca: "Intel",
+        valor: 800,
+        srcImg: "assets/cpus/i310100f.png"
+    },
+    {
+        id:3,
+        nome: "Ryzen 5 4600G",
+        marca: "AMD",
+        valor: 900,
+        srcImg: "assets/cpus/ryzen5.jpg"
+    },
+    {
+        id:4,
+        nome: "Ryzen 5 5600G",
+        marca: "AMD",
+        valor: 1200,
+        srcImg: "assets/cpus/ryzen5.jpg"
+    },
+    {
+        id:5,
+        nome: "GTX 1660 Super",
+        marca: "MSI",
+        valor: 1200,
+        srcImg: "assets/gpus/1660s.jpg"
+    },
+    {
+        id:6,
+        nome: "RTX 2060",
+        marca: "GIGABYTE",
+        valor: 1500,
+        srcImg: "assets/gpus/2060.jpg"
+    },
+    {
+        id:7,
+        nome: "RTX 4090",
+        marca: "AORUS",
+        valor: 5000,
+        srcImg: "assets/gpus/4090.jpg"
+    },
+    {
+        id:9,
+        nome: "RX 580",
+        marca: "GIGABYTE",
+        valor: 600,
+        srcImg: "assets/gpus/rx580.png"
+    },
+    
+]
